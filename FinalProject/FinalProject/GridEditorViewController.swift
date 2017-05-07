609b3ee223be2a4b7b493ab164ef56821aa700d4
Doug Newman <doug@dougnewman.org>
2017-05-07 18:36:55 -0400

    Instantiated Grid in Grid Editor View

--- FinalProject/FinalProject/GridEditorViewController.swift
+++ FinalProject/FinalProject/GridEditorViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class GridEditorViewController: UIViewController {
+class GridEditorViewController: UIViewController, GridViewDataSource {
     
     @IBOutlet weak var editorGridView: GridView!
     
@@ -16,16 +16,20 @@ class GridEditorViewController: UIViewController {
     
     var gridName: String = ""
     var gridArray: [[Int]] = []
+    var engine = StandardEngine(rows: 10, cols: 10)
     var saveClosure: ((String) -> Void)?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        editorGridView.gridDataSource = self
         let gridArrayMax = (Array(gridArray.joined()).max()! * 2)
         if (gridArrayMax >= 10) {
             self.editorGridView.gridSize = gridArrayMax
+            engine.grid = Grid(gridArrayMax, gridArrayMax)
         }
         else {
             self.editorGridView.gridSize = 10
+            engine.grid = Grid(10, 10)
         }
     }
     
@@ -33,6 +37,11 @@ class GridEditorViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
+    public subscript (row: Int, col: Int) -> CellState {
+        get { return engine.grid[row,col] }
+        set { engine.grid[row,col] = newValue }
+    }
+
     @IBAction func saveButton(_ sender: UIBarButtonItem) {
         
     }
