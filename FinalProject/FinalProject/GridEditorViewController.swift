188ae2eac1ae4e4dab9e748a2d6f6b2de7a8b7d3
Doug Newman <doug@dougnewman.org>
2017-05-07 20:25:54 -0400

    Enabled publishing Grid from Grid Editor View to Simulation View

--- FinalProject/FinalProject/GridEditorViewController.swift
+++ FinalProject/FinalProject/GridEditorViewController.swift
@@ -21,6 +21,7 @@ class GridEditorViewController: UIViewController, GridViewDataSource {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.gridNameTextField.text = gridName
         editorGridView.gridDataSource = self
         let gridArrayMax = (Array(gridArray.joined()).max()! * 2)
         if (gridArrayMax >= 10) {
@@ -41,9 +42,13 @@ class GridEditorViewController: UIViewController, GridViewDataSource {
         get { return engine.grid[row,col] }
         set { engine.grid[row,col] = newValue }
     }
-
+    
     @IBAction func saveButton(_ sender: UIBarButtonItem) {
-        
+        StandardEngine.engine.grid = engine.grid
+        let nc = NotificationCenter.default
+        let name = Notification.Name(rawValue: "EngineUpdate")
+        let n = Notification(name: name, object: nil, userInfo: ["engine" : self])
+        nc.post(n)
     }
     
 }
